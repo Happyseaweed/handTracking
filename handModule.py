cccdc658a40f5d5a019b4108af9f51b569f7d9fd
Siwei Du <seaweedgo@gmail.com>
2022-08-23 15:30:16 -0500

    Added swipe direction feature, cleaned up UI and added an ungodly amount of comments.

--- handModule.py
+++ handModule.py
@@ -1,3 +1,4 @@
+from turtle import Turtle
 import mediapipe as mp
 import cv2 as cv
 import numpy as np
@@ -7,22 +8,31 @@ import time
 
 
 class HandDetector():
-    def __init__(self, mode = False, maxHands = 2, detectionCon= 0.5, trackCon = 0.5):
+    def __init__(self, mode = False, maxHands = 2, detectionCon= 0.5, trackCon = 0.5, grabbed = False, grabPos = []):
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
         self.trackCon = trackCon
 
+        # Mediapipe hands solution, might need to be updated 
+        # as Mediapipe gets updated in the future
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands(
                                         self.mode,
                                         self.maxHands,
+                                        1,
                                         self.detectionCon,
                                         self.trackCon
                                         )
         self.mp_draw = mp.solutions.drawing_utils
 
+        # Variables for swipe direction function
+        self.grabbed = grabbed
+        self.grabPos = grabPos
+        self.direction = "None"
+
     def findHands(self, img, draw = True):
+        # Make img RGB so it can be used in the process function
         imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
 
@@ -36,17 +46,25 @@ class HandDetector():
                         self.mp_draw.DrawingSpec(color=(207,252,3), thickness=5, circle_radius=3),
                         self.mp_draw.DrawingSpec(color=(255,255,255), thickness=5, circle_radius=1)
                     )
-            centers = self.center_of_mass(img, 0)
-            palm = self.palm_center(img, 0)
-            # Center of mass and palm center
-            cv.circle(img, (centers[0][0], centers[0][1]), 3, (0, 0, 255), 5)
-            cv.circle(img, (palm[0][0], palm[0][1]), 3, (255, 0, 0), 5)
 
         return img
     
     def findPosition(self, img, handNo = 0, draw = True):
+        """findPosition()
+
+        Args:
+            img:    an img to process and find hands on
+            handNo: number of hands on the screen
+            draw:   Used for debugging purposes
+        Returns:
+            list: a list of coordinates representing the points on a hand
+                  according to this image:
+                  https://google.github.io/mediapipe/images/mobile/hand_landmarks.png    
+        """
+        
         self.retList = []
 
+        # Can be improved...
         if handNo == 2:
             if self.results.multi_hand_landmarks:
                 for hands in self.results.multi_hand_landmarks:
@@ -55,9 +73,6 @@ class HandDetector():
                         # h, w = 1, 1
                         cx, cy = int(w*lm.x), int(h*lm.y)
                         self.retList.append([ind, cx, cy])
-                    if draw:
-                        cv.putText(img, "Both Hands", (50, 70), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 1)
-
         else:
             if self.results.multi_hand_landmarks:
                 currentHand = self.results.multi_hand_landmarks[handNo]
@@ -67,32 +82,56 @@ class HandDetector():
                     cx, cy = int(w*lm.x), int(h*lm.y)
                     self.retList.append([ind, cx, cy])
 
-                    if draw:
-                        cv.putText(img, ("Hand: " + str(handNo)),(50, 70), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 1)
-            
         return self.retList
 
     def fingerCount(self, img, handNo = 0, draw = True):
+        """fingerCount()
+
+        Args:
+            img:    an image to process and find the hands on.
+            handNo: number of hands on screen.
+            draw:   debugging purposes.
+        Returns:
+            list: a list of numbers representing each finger
+        
+        Other Variables:
+            SHIFT: which joint is used in comparison against the fingertips
+                   when running the algorithm.
+                   The algorithm works by determining whether the fingertip to palm
+                   distance is shorter than the specified joint to palm distance.
+                   In this case, we are using the 1st joint from the knuckles with
+                   SHIFT = 2
+        """
+
+        # thumb: 4 = tip
         # index: 8 = tip, 5 = knuckle
         # middle: 12 = tip, 9 = knuckle
         total = 0
         indexList = []
+
+        SHIFT = 2;
+
+        # Making sure there are hands within the camera frame first.
         if self.results.multi_hand_landmarks:
-            center = self.center_of_mass(img, handNo)    
+            # Palm and center of mass coordinates.
+            # center = self.center_of_mass(img, handNo)    
             palm_c = self.palm_center(img, handNo)
 
+            # Calculating the distances for index to pinky.
             for i in range(8, 24, 4):
                 distA = self.dist(palm_c[0][0], palm_c[0][1], self.retList[i][1], self.retList[i][2])
-                distB = self.dist(palm_c[0][0], palm_c[0][1], self.retList[i-2][1], self.retList[i-2][2])
+                distB = self.dist(palm_c[0][0], palm_c[0][1], self.retList[i-SHIFT][1], self.retList[i-SHIFT][2])
                 # cv.circle(img, (self.retList[i][1], self.retList[i][2]), 10, (255, 255, 255), 10)
                 
-                print(i," ",distA, " " ,distB)
+                # print(i," ",distA, " " ,distB)
                 if distA > distB:
                     indexList.append(i)
                     total+=1
 
+            # Calculating thumb, separate from others due to physical differences.
+            # So, thumb might need to be treated differently in the future.
             distA = self.dist(palm_c[0][0], palm_c[0][1], self.retList[4][1], self.retList[4][2])
-            distB = self.dist(palm_c[0][0], palm_c[0][1], self.retList[2][1], self.retList[2][2])
+            distB = self.dist(palm_c[0][0], palm_c[0][1], self.retList[4-SHIFT][1], self.retList[4-SHIFT][2])
             
             if (distA > distB):
                 indexList.append(4)
@@ -101,10 +140,23 @@ class HandDetector():
         return indexList
 
     def checkGrab(self, img, handNo = 0, draw = True):
+        """checkGrab()
+
+        This function works by calculating the avg point of the finger tips
+        and seeing if the distance from each fingertip is within a certain distance
+        to that average point.
+
+        Args:
+            img:    an image to process
+            handNo: number of hands in frame
+            draw:   Debugging purposes.
+        Returns:
+            Boolean: if the hand in frame is making a grabbing gesture.    
+        """
+
+
         avg_x = 0
         avg_y = 0
-        length = img.shape[1]
-        width = img.shape[0]
 
         finger_tips = []
 
@@ -116,21 +168,150 @@ class HandDetector():
                 finger_tips.append(hand_landmarks.landmark[16])
                 finger_tips.append(hand_landmarks.landmark[20])
 
-        for points in finger_tips:
-            avg_x += points.x
-            avg_y += points.y
-        
-        avg_x = avg_x/5
-        avg_y = avg_y/5
+            for points in finger_tips:
+                avg_x += points.x
+                avg_y += points.y
+            
+            avg_x = avg_x/5
+            avg_y = avg_y/5
 
-        for i in range(0, 5):
-            dist = math.sqrt(math.pow(finger_tips[i].x-avg_x, 2) + math.pow(finger_tips[i].y-avg_y, 2));
-            if dist >= 0.06:
-                return False
+            for i in range(0, 5):
+                dist = math.sqrt(math.pow(finger_tips[i].x-avg_x, 2) + math.pow(finger_tips[i].y-avg_y, 2));
+                if dist <= 0.02:
+                    return True
 
-        return True
+        return False
     
+    def checkGrabAlt(self, img, handNo = 0, draw = True, debug = False):
+        """checkGrabAlt()
+
+        This function is an alternative way of checking for grabbing gesture.
+        It is much simpler and easier to understand compared to the original.
+        But I will keep the original because I am too lazy to delete it and it 
+        might become helpful in the future.
+        
+        This function works by checking the distance between the center of the palm
+        and the center of mass of the hand to see if they are within a certain
+        distance of each other.
+
+        Args:
+            img:    an image to process
+            handNo: number of hands in frame
+            draw:   debugging purposes
+            debug:  even more debugging purposes (I need to clean this up soon)
+        Returns:
+            Boolean: if the hand is making a grabbing gesture.
+        """
+
+        if self.results.multi_hand_landmarks:
+            mass = self.center_of_mass(img, 0)
+            cent = self.palm_center(img, 0)
+
+            if debug:
+                print("+-----CheckGrabAlt DEBUG-----+")
+                print("mass: ", mass)
+                print("cent: ", cent)
+                print("dist: ", self.dist(mass[0][0], mass[0][1], cent[0][0], cent[0][1]))
+
+
+            if self.dist(mass[0][0], mass[0][1], cent[0][0], cent[0][1]) < 100:
+                return True
+
+        return False
+
+    def swipeDirection(self, img, debug = False):
+        """swipeDirection()
+
+        Concept: To determine the direction swiped, we use the 
+        release position relative to the initial grab position
+        
+        We can set the initial grab position as the origin of a 
+        cartesian plane, and split the plane into 4 regions with 2
+        lines that form an 'X'. Which ever region the release position
+        lands in is the direction being swiped to.
+
+        The 'X' will be formed by equations: y = +-(height/width)x
+        we just need to check whether the finished y position is above 
+        or below each of the two lines. From this we can easily determine 
+        which region the hand ended up in
+
+        Args:
+            img:    an image to process on
+            debug:  debugging purposes.
+        Returns:
+            string: the direction of the grab-swipping gesture
+
+        """
+        ret = "None"
+        
+        if not self.results.multi_hand_landmarks:
+            return ret
+
+        # This is a toggle structure.
+        if self.checkGrab(img):
+            if self.grabbed == False:
+                self.grabbed = True
+                self.grabPos = self.center_of_mass(img, 0)
+        else:
+            if self.grabbed == True:
+                self.grabbed = False
+                
+                # Calculate direction:
+                # Getting final position of hand when releasing grab
+                finalPos = self.center_of_mass(img, 0)
+                
+                grab_x = self.grabPos[0][0]
+                grab_y = self.grabPos[0][1]
+                fin_x = finalPos[0][0]
+                fin_y = finalPos[0][1]
+
+                width = img.shape[1]
+                height = img.shape[0]
+
+                # Set initial position as origin:
+                fin_x -= grab_x
+                fin_y -= grab_y
+
+                # Variables for determine which region, 
+                # (a, b): (1, 1) top, (1, 0) left, (0, 1) right, (0, 0) bottom
+                a = -1
+                b = -1
+
+                if fin_y >= (height/width) * fin_x:
+                    a = 1;
+                else:
+                    a = 0
+                if fin_y >= -(height/width) * fin_x:
+                    b = 1;
+                else:
+                    b = 0
+
+                # Camera is mirror, so go opposite
+                if a == 1:
+                    if b == 1:
+                        ret = "Down"
+                    else:
+                        ret = "Right"                
+                else:
+                    if b == 1:
+                        ret = "Left"
+                    else:
+                        ret = "Up"
+
+        return ret
+
     def center_of_mass(self, img, handNo): #handNo: 0 = first hand, 1 = second hand
+        """center_of_mass()
+        
+        Args:
+            img:    An image to process on
+            handNo: Number of hands in frame.
+        Returns:
+            list: coordinates of the center of mass of the hand based on
+                  the landmarks.
+
+        """
+        
         ret = []
         if self.results.multi_hand_landmarks:
             hands = self.results.multi_hand_landmarks[handNo]
@@ -146,6 +327,15 @@ class HandDetector():
         return ret
 
     def palm_center(self, img, handNo):
+        """palm_center()
+        
+        Args:
+            img:    An image to process on
+            handNo: number of hands in frame.
+        Returns:
+            list: coordinates of the center of palm
+
+        """
         ret = []
         if self.results.multi_hand_landmarks:
             hands = self.results.multi_hand_landmarks[handNo]
@@ -155,31 +345,61 @@ class HandDetector():
             for ind in range(5, 21, 4):
                 avg_x += hands.landmark[ind].x * img.shape[1]
                 avg_y += hands.landmark[ind].y * img.shape[0]
-            avg_x += hands.landmark[0].x * img.shape[1]
-            avg_y += hands.landmark[0].y * img.shape[0]
-            avg_x /= 5
-            avg_y /= 5
+
+            # 2 Times for better accuracy
+            avg_x += 2 * hands.landmark[0].x * img.shape[1]
+            avg_y += 2 * hands.landmark[0].y * img.shape[0]
+
+            avg_x /= 6
+            avg_y /= 6
             ret.append([int(avg_x), int(avg_y)])
         
         return ret
 
     def dist(self, x1, y1, x2, y2):
+        """dist()
+
+        Args:
+            x1: x position of first point
+            y1: y position of first point
+            x2: x position of second point
+            y2: y position of second point
+        Returns:
+            float: a number in pixels representing the distance.
+        """
         return math.sqrt(math.pow(x1-x2, 2) + math.pow(y1-y2, 2))
 
     def help(self):
-        print("OpenCV x Mediapipe Hand Tracking Module made by Siwei Du")
+        """help()
+        
+        A helper function designed to print out all available features of
+        the module as well as some helpful tips with using the module.
+
+        """
+        print("\n\n\nOpenCV x Mediapipe Hand Tracking Module made by Siwei Du")
         print("+--------------------------------------------------------------------------------------------------------------------+")
         print("Functions: ")
         print("findHands():\t\tFinds and plots the skeletal structure on image.[Returns an Image]")
         print("findPosition():\t\tFinds the position of each hand landmark.[Returns a list]")
         print("fingerCount():\t\tCounts how many fingers are held up. Works best with upright hand.[Returns a list]")
         print("checkGrab():\t\tChecks if the hand makes a grab gesture.[Returns a boolean]")
+        print("checkGrabAlt():\t\tAlternative method for checkGrab(). [Returns a boolean]")
+        print("swipeDirection():\t\tChecks which way the user swipes their hand while grabbing. [Returns a string]")
         print("center_of_mass():\tFinds the center of mass of the hand, indicated by a red dot.[Returns a list]")
         print("palm_center():\t\tFinds the center of the palm, indicated by a blue dot.[Returns a list]")
         print("dist():\t\t\tFinds the distance between two points.[Returns a float]")
-        print("+--------------------------------------------------------------------------------------------------------------------+")
-        
+        print("+--------------------------------------------------------------------------------------------------------------------+\n>>\n")
+        print("!!!!!!!!!!\tREAD FOR CLARIFICATION\t!!!!!!!!!!")
+        print("* For maximum accuracy, make sure environment is: ")
+        print("*** Well lit, palm facing camera, single hand in frame.\n\n")
+        print("* Swipe Direction:")
+        print("*** To use, grab hand, move hand, release hand.")
+        print("*** Grab gesture works best with fist, or pinching all your fingers together")
+        print("*** so that the fingertips are as close as possible.")
+        print("*** Works best if you exaggerate the grab and release gestures\n\n")
 
+
+# ----------------------------- Main 
 def main():
     ptime = 0
     ctime = 0
@@ -214,8 +434,6 @@ def main():
 
         if cv.waitKey(1) == 27:     # ESC to break
             break
-
-
 
 if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# ----------------------------- Include files
 import mediapipe as mp
 import cv2 as cv
 import numpy as np
@@ -5,7 +6,7 @@ import math
 import time
 import handModule as hm
 
-####################################
+# ----------------------------- Global Variables
 
 WIDTH = 1280
 HEIGHT = 920
@@ -13,7 +14,13 @@ HEIGHT = 920
 ptime = 0
 ctime = 0
 
-####################################
+FINGERCOUNTER   = False
+CENTEROFMASS    = False
+CENTEROFPALM    = False
+CHECKGRAB       = False
+SWIPEDIRECTION  = False
+
+# ----------------------------- OpenCV & Module initiation
 
 cap = cv.VideoCapture(0)
 
@@ -25,22 +32,80 @@ while cap.isOpened():
 
     success, frame = cap.read()
     
-    frame = cv.resize(frame, (WIDTH, HEIGHT));
+    # frame = cv.resize(frame, (frame.shape[0], HEIGHT));
+    # frame = cv.flip(frame, 1)
 
     img = detector.findHands(frame)
-    detector.findPosition(frame)
+    detector.findPosition(frame, 2)
+    hasHands = detector.results.multi_hand_landmarks
+
     # img = cv.flip(img, 1)
 
-    fCnt = detector.fingerCount(frame)
-
+    # -----------------------------FPS Calculations:
     ctime = time.time()
     fps = 1 / (ctime-ptime)
     ptime = ctime
 
+    # -----------------------------Menu Display:
     cv.putText(img, "FPS: " + str(int(fps)), (50, 50), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-    cv.putText(img, "Finger Count: " + str(int(len(fCnt))), (650, 50), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv.putText(img, "[G] Check Grab", (50, 100), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv.putText(img, "[F] Finger Count", (50, 150), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv.putText(img, "[M] Center of Mass (Red)", (50, 200), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv.putText(img, "[P] Palm Center (Blue)", (50, 250), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv.putText(img, "[D] Swipe Direction", (50, 300), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
+    # -----------------------------Drawing Features:
+    if FINGERCOUNTER == True:
+        fCnt = detector.fingerCount(frame)
+        cv.putText(img, "[Finger Count: " + str(int(len(fCnt))) + "]", (600, 50), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    
+    if CHECKGRAB == True:
+        state = detector.checkGrabAlt(frame, debug = False)
+        cv.putText(img, "[Grabbing: " + str(int(state)) + "]", (1050, 50), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+    if CENTEROFMASS == True and hasHands:
+        centers = detector.center_of_mass(frame, 0)
+        cv.circle(img, (centers[0][0], centers[0][1]), 3, (0, 0, 255), 10)
+    
+    if CENTEROFPALM == True and hasHands:
+        palm = detector.palm_center(frame, 0)
+        cv.circle(img, (palm[0][0], palm[0][1]), 3, (255, 0, 0), 10)
+
+
+    if SWIPEDIRECTION == True:
+        direction = detector.swipeDirection(frame)
+        cv.putText(img, "[Direction: " + detector.direction + "]", (1390, 50), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+        
+        if direction != "None":
+            detector.direction = direction
+            print(direction)
+
+    # -----------------------------Displaying
     cv.imshow('Video', img)
 
-    if cv.waitKey(1) == 27:     # ESC to break
+    # -----------------------------Toggling features for demonstration purposes.
+    KEY = cv.waitKey(5)
+
+    if KEY & 0xFF == ord('f'):
+        FINGERCOUNTER = not FINGERCOUNTER
+
+    if KEY & 0xFF == ord('g'):
+        CHECKGRAB = not CHECKGRAB
+
+    if KEY & 0xFF == ord('m'):
+        CENTEROFMASS = not CENTEROFMASS
+
+    if KEY & 0xFF == ord('p'):
+        CENTEROFPALM = not CENTEROFPALM
+    
+    if KEY & 0xFF == ord('d'):
+        SWIPEDIRECTION = not SWIPEDIRECTION
+        detector.direction = "None"
+        
+    # -----------------------------Exit Condition
+    if KEY == 27:     # ESC to break
         break
+
+# ----------------------------- OpenCV release & destroy
+cap.release()
+cv.destroyAllWindows()
